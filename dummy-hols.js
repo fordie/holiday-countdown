9677d34ef057de1bb21a0fa6a924aa6148d7904c
Mark Ford <mark@intelligentli.me>
2018-03-07 16:03:59 +0000

    dummy holiday dates

---
+++ dummy-hols.js
@@ -0,0 +1,16 @@
+{
+	"holidays": [
+		{
+			"destination": "The moon",
+			"departure": "2019, 1, 15"
+		},
+		{
+			"destination": "The Past",
+			"departure": "2015, 1, 11"
+		},
+		{
+			"destination": "Uranus",
+			"departure": "2018, 4, 22"
+		}
+	]
+}
